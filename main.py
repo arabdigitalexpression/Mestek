343eeb3946d776b4bc0e5582002247bd4320392c
Ahmed Ramadan <ahmedrs@protonmail.com>
2022-05-29 20:14:17 +0200

    Flask init

---
+++ main.py
@@ -0,0 +1,4 @@
+# This is the start of our program (See the .flaskenv file).
+# We import the app (global) object from the app module (see __init__.py)
+# the app object starts our Flask project.
+from app import app
