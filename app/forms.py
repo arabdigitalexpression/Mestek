0855d88e8f30af45598005ae57a69aa1713604ff
Ahmed Ramadan <ahmedrs@protonmail.com>
2022-06-17 19:37:53 +0200

    Added CreateSpaceForm
    Added temp github version of flask-uploads

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, email
+from flask_uploads import UploadSet, IMAGES
+from wtforms import (
+    StringField, PasswordField, EmailField, SubmitField,
+    FloatField, BooleanField, TextAreaField
+)
+from wtforms.validators import (
+    DataRequired, Length, EqualTo, email
+)
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+
+images = UploadSet('images', IMAGES)
 
 
 class LoginForm(FlaskForm):
@@ -22,3 +32,15 @@ class SignupForm(FlaskForm):
     password = PasswordField('password', validators=[DataRequired(),Length(min=6,max=20)])
     confirm_password = PasswordField('password', validators=[DataRequired(),EqualTo('password')])
     submit= SubmitField('تسجيل الحساب')
+
+
+class CreateSpaceForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired(), Length(max=50)])
+    price = FloatField('price', validators=[DataRequired()])
+    has_operator = BooleanField('has operator', validators=[DataRequired()])
+    description = TextAreaField('description', validators=[DataRequired(), Length(max=128)])
+    guidelines = TextAreaField('guidelines', validators=[DataRequired(), Length(max=256)])
+    images = FileField('images', validators=[
+        FileRequired(),
+        FileAllowed(images, 'Images only!')
+    ])
