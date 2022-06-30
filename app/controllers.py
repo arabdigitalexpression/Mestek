ad48a9a7a9c104cc639693c28e214b9b010f8966
Ahmed ramadan <2356950-Ahmed_Ramadan@users.noreply.gitlab.com>
2022-06-30 02:21:18 +0200

    Added dashboard and space controllers
    Added mainpage controller of dashboard
    Added list and delete controller of space
    Improved syntax of models and forms import lines
    Added 2 spaces and 1 image in test.db

--- app/controllers.py
+++ app/controllers.py
@@ -1,6 +1,12 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import current_user, login_user, logout_user, login_required
-from app import app, models, forms, login
+from app import app, db, login
+from app.models import (
+    User, Space, Image
+)
+from app.forms import (
+    SignupForm, LoginForm, CreateSpaceForm
+)
 
 
 # TODO: Remove this unused stuff
@@ -19,7 +25,7 @@ def main_page():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup_page():
-    form = forms.SignupForm()
+    form = SignupForm()
     if request.method == "POST":
         if form.validate_on_submit():
             username = form.userName.data
@@ -27,11 +33,11 @@ def signup_page():
             password = form.password.data
             # TODO: get category input from user
 
-            user = models.User.query.get(email)
+            user = User.query.get(email)
 
             if user == None:
                 # TODO: Edit the category with user input
-                user = models.User(
+                user = User(
                     username=username,
                     email=email,
                     role="user",
@@ -55,14 +61,14 @@ def login_page():
     if current_user.is_authenticated:
         return redirect(url_for("main_page"))
     
-    form = forms.LoginForm()
+    form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
 
-        # user = models.User.query.get()
+        # user = User.query.get()
         # filter users by username and then get first one
-        user = models.User.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=username).first()
 
         print(f"login username: {username}")
         print(f"login user object: {user}")
@@ -83,3 +89,23 @@ def login_page():
 def logout():
     logout_user()
     return redirect(url_for('main_page'))
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template("dashboard/index.html")
+
+
+@app.route('/spaces')
+def space_list():
+    data = Space.query.all()
+    return render_template('dashboard/spaces.html', spaces=data)
+
+
+@app.route('/space/<int:id>', methods=['POST'])
+def delete_space(id):
+    space = Space.query.get(id)
+    db.session.delete(space)
+    db.session.commit()
+    return redirect(url_for("space_list"))
