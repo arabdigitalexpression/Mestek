0ef89b4d614e9d9abed63f9c3d22d7b86f9adc9d
mostafa <mostafa74129@gmail.com>
2022-09-22 17:43:16 +0200

    removed unnecessary style code

--- app/static/dashboard.js
+++ app/static/dashboard.js
@@ -127,82 +127,29 @@ function upto(){
 }
 
 
-// $(".content").hide();
-// $(document).ready(function () {
-//   $("#show_hide").on("click", function () {
-//     $(this).next('.content').slideToggle(200);
-//   });
-//   $("#selUser").select2();
-// });
-
-
-// $('.select').each(function () {
-//   var $this = $(this), numberOfOptions = $(this).children('option').length;
-
-//   $this.addClass('select-hidden');
-//   $this.wrap('<div class="select"></div>');
-//   $this.after('<div class="select-styled"></div>');
-
-//   var $styledSelect = $this.next('div.select-styled');
-//   $styledSelect.text($this.children('option').eq(0).text());
-
-//   var $list = $('<ul />', {
-//     'class': 'select-options'
-//   }).insertAfter($styledSelect);
-
-//   for (var i = 0; i < numberOfOptions; i++) {
-//     $('<li />', {
-//       text: $this.children('option').eq(i).text(),
-//       rel: $this.children('option').eq(i).val()
-//     }).appendTo($list);
-//     //if ($this.children('option').eq(i).is(':selected')){
-//     //  $('li[rel="' + $this.children('option').eq(i).val() + '"]').addClass('is-selected')
-//     //}
-//   }
-
-//   var $listItems = $list.children('li');
-
-//   $styledSelect.click(function (e) {
-//     e.stopPropagation();
-//     $('div.select-styled.active').not(this).each(function () {
-//       $(this).removeClass('active').next('ul.select-options').hide();
-//     });
-//     $(this).toggleClass('active').next('ul.select-options').toggle();
-//   });
-
-//   $listItems.click(function (e) {
-//     e.stopPropagation();
-//     $styledSelect.text($(this).text()).removeClass('active');
-//     $this.val($(this).attr('rel'));
-//     $list.hide();
-//     //console.log($this.val());
-//   });
-
-
-// });
-
 function space_validate(){
-//   if(document.form.spaceName.value=="hide")
-// {
-// alert ( "برجاء اختيار مساحة");
-// return false;
-// }
-// if(document.form.type.value=="hide")
-// {
-// alert ( "برجاء تحديد نوع الحجز");
-// return false;
-// }
-// else if (document.form.date_from_to.value =="" && document.form.date_from_to_no_range.value ==""){
-//   alert ( "برجاء تحديد ميعاد الحجز");
-//   return false; 
-// }else if (document.form.time_picker_no_range.value =="" && document.form.time2_picker_no_range.value =="" ){
-//   if (document.form.date_from_to_no_range.value !=""){
-//   alert ( "برجاء تحديد توقيت الحجز");
-//   return false;
-// }else if (document.form.date_from_to_no_range.value !=""){
-//   return true;
-// }
-// }
+  
+  if(document.form.spaceName.value=="hide")
+{
+alert ( "برجاء اختيار مساحة");
+return false;
+}
+if(document.form.type.value=="hide")
+{
+alert ( "برجاء تحديد نوع الحجز");
+return false;
+}
+else if (document.form.date_from_to.value =="" && document.form.date_from_to_no_range.value ==""){
+  alert ( "برجاء تحديد ميعاد الحجز");
+  return false; 
+}else if (document.form.time_picker_no_range.value =="" && document.form.time2_picker_no_range.value =="" ){
+  if (document.form.date_from_to_no_range.value !=""){
+  alert ( "برجاء تحديد توقيت الحجز");
+  return false;
+}else if (document.form.date_from_to_no_range.value !=""){
+  return true;
+}
+}
 }
 
 function tool_validate(){
