44e4b4f08eb5664be8b6ab02eb440cf18af0b70f
Dev Joe <joe.afify95@gmail.com>
2022-12-11 13:29:41 +0200

    Activated reservations getter api action for admin calendar

--- app/static/js/calender.js
+++ app/static/js/calender.js
@@ -1,61 +1,13 @@
-let events = [
-{
-                title: 'Business Lunch',
-                start: '2020-09-03T13:00:00',
-                constraint: 'businessHours'
-            },
-            {
-                title: 'Meeting',
-                start: '2020-09-13T11:00:00',
-                constraint: 'availableForMeeting', // defined below
-                color: '#257e4a'
-            },
-            {
-                title: 'Conference',
-                start: '2020-09-18',
-                end: '2020-09-20'
-            },
-            {
-                title: 'Party',
-                start: '2020-09-29T20:00:00'
-            }, {
-                title: 'Party',
-                start: '2020-09-29T21:00:00'
-            },
+let events = []
 
-            // areas where "Meeting" must be dropped
-            {
-                groupId: 'availableForMeeting',
-                start: '2020-09-11T10:00:00',
-                end: '2020-09-11T16:00:00',
-                display: 'background'
-            },
-            {
-                groupId: 'availableForMeeting',
-                start: '2020-09-13T10:00:00',
-                end: '2020-09-13T16:00:00',
-                display: 'background'
-            },
-
-            // red areas where no events can be dropped
-
-            {
-                start: '2020-09-25',
-                end: '2020-09-26',
-                overlap: false,
-                display: 'background',
-                color: '#ff9f89'
-            }]
-
-myData()
-
-async function myData(){
-   const res = await fetch('http://127.0.0.1:5000/api/dashboard/reservations/')
-   const data = await res.json()
-   console.log(data)
+let getReservations = async () => {
+    const res = await fetch('http://127.0.0.1:5000/api/reservations/');
+    const data = await res.json();
+    events = data
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', async function () {
+    await getReservations()
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -78,6 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
         },
         selectable: true,
         events: events
+
     });
     calendar.render();
 });
