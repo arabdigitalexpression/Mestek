66cb3f33b3ea187bf6e0f9b21054b4b95668abac
Dev Joe <joe.afify95@gmail.com>
2023-03-29 17:24:38 +0200

    Add gender input

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,6 +6,7 @@ from wtforms import (
 from wtforms.validators import (
     DataRequired, Length, EqualTo
 )
+from app.enums import Gender
 
 
 class LoginForm(FlaskForm):
@@ -13,13 +14,13 @@ class LoginForm(FlaskForm):
                            render_kw={
                                "class": "form-control form-control-user",
                                "placeholder": "إسم المستخدم"
-                           })
+    })
     password = PasswordField('كلمة المرور', validators=[DataRequired()],
                              render_kw={
                                  "class": "form-control form-control-user",
                                  "placeholder": "كلمة المرور"
 
-                             })
+    })
 
     # our own custom validator for the length
     def validate_user_is_unique(form, field):
@@ -33,17 +34,17 @@ class SignupForm(FlaskForm):
                             render_kw={
                                 "class": "form-control form-control-user",
                                 "placeholder": "الاسم الأول"
-                            })
+    })
     lastName = StringField('الاسم الأخير', validators=[DataRequired(), Length(min=3, max=20)],
                            render_kw={
                                "class": "form-control form-control-user",
                                "placeholder": "الاسم الأخير"
-                           })
+    })
     userName = StringField('إسم المستخدم', validators=[DataRequired(), Length(min=3, max=20)],
                            render_kw={
                                "class": "form-control form-control-user",
                                "placeholder": "إسم المستخدم"
-                           })
+    })
     phone = StringField(
         'رقم الهاتف', validators=[DataRequired(), Length(min=11, max=20)],
         render_kw={
@@ -51,23 +52,28 @@ class SignupForm(FlaskForm):
             "placeholder": "رقم الهاتف"
         }
     )
+    gender = SelectField('الجنس', validators=[DataRequired()],
+                         choices=[(Gender.male.name, Gender.male.description),
+                                  (Gender.female.name, Gender.female.description),
+                                  (Gender.prefer_not_answer.name, Gender.prefer_not_answer.description)],
+                         render_kw={
+                             "class": "form-select form-control-user p-3",
+                             "placeholder": "النوع"
+    })
     email = EmailField('البريد الإلكترونى', validators=[DataRequired()],
                        render_kw={
                            "class": "form-control form-control-user text-start",
                            "placeholder": "البريد الإلكترونى"
-                       })
+    })
     password = PasswordField('كلمة المرور', validators=[DataRequired(), Length(min=6, max=20)],
                              render_kw={
                                  "class": "form-control form-control-user",
                                  "placeholder": "كلمة المرور"
-                             })
+    })
     confirm_password = PasswordField('تأكيد كلمة المرور', validators=[DataRequired(), EqualTo('password')],
                                      render_kw={
                                          "class": "form-control form-control-user",
                                          "placeholder": "تأكيد كلمة المرور"
-                                     })
-    role = SelectField('الصلاحية', render_kw={
-        "class": "form-select form-control-user",
     })
     category = SelectField('التصنيف', render_kw={
         "class": "form-select form-control-user p-3",
